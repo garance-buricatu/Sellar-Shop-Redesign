619321318b7be9bf8c0c9b62c9c55934791bb865
Garance Buricatu <garance.mary@gmail.com>
2021-01-04 14:26:49 -0500

    connecting to mongo db with mongoose

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config'); // config packaege allows for global variables that will be used throughout application
+const db = config.get('mongoURI');
+
+const connectDB = async () => { // async/await always wrapped isnide try catch block
+    try {
+        //mongoose.connect returns a promise
+        await mongoose.connect(db, { // pass object for deprecation
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }); 
+        console.log('MongoDB connected...');
+    
+    } catch (err) { // MongoDB unable to connect
+        console.log(err.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
