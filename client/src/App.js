c2b80f08492a114d4323442521ba426996c36cf1
Garance Buricatu <garance.mary@gmail.com>
2021-01-06 21:13:43 -0500

    route and frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import './styles/App.scss';
+
+import Navbar from './components/layout/Navbar';
+
+const App = () => 
+    <Fragment className="App">
+      <Navbar></Navbar>
+    </Fragment>
+
+export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Navbar = props => {
+    return (
+        <div className="navbar">
+            <div className="dark-overlay">
+                <div className="navbar-inner">
+                    <h1 className="x-large">The Sellar Shop</h1>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+Navbar.propTypes = {
+
+}
+
+export default Navbar
